d2a6be04a5d002bbcdb0b9607b7d6a121eba0faa
CodeNur <dodgechalanger9@gmail.com>
2025-11-22 22:11:49 +0500

    Add new UI components and animations; update dependencies and styles

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,8 +1,12 @@
 import Social from "./Social";
 import { buttonVariants } from "./ui/button";
+import { Globe } from "./ui/globe";
 import MeImg from "/public/mee.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import { WordRotate } from "./ui/word-rotate";
+import { Highlighter } from "./ui/highlighter";
+import { HyperText } from "./ui/hyper-text";
 
 export default function Me() {
   return (
@@ -23,34 +27,38 @@ export default function Me() {
               <h2 className="text-2xl font-medium mb-1 sm:text-4xl">
                 MuhammadNurulloh
               </h2>
-              <p className="text-slate-500 text-lg font-medium mb-2 sm:text-2xl">
-                <a
-                  href="https://www.google.com/search?q=front-end+developer+%D1%87%D1%82%D0%BE+%D0%B4%D0%B5%D0%BB%D0%B0%D0%B5%D1%82&client=ubuntu-chr&hs=kq8&sca_esv=ec4e310bc6b52ebd&sxsrf=ADLYWIJlV0cAG-fqXvYBocOuH-Zq2iy2Nw%3A1736950557525&ei=HcOHZ67nH-uxwPAPvJyEqAM&oq=frontend+who&gs_lp=Egxnd3Mtd2l6LXNlcnAiDGZyb250ZW5kIHdobyoCCAEyBxAjGLADGCcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEcyChAAGLADGNYEGEdI_zhQAFgAcAF4AJABAJgBfKABfKoBAzAuMbgBA8gBAJgCAaACCZgDAIgGAZAGCZIHATGgB6sF&sclient=gws-wiz-serp"
-                  target="_blank"
-                >
-                  FrontEnd
-                </a>{" "}
-                dasturchi
-              </p>
+              <span className="text-slate-500 text-lg font-medium mb-2 sm:text-2xl">
+                <WordRotate words={["FrontEnd dasturchi", "ReactJs, NextJS"]} />
+              </span>
+
               <Social />
             </div>
           </div>
           <div className="flex flex-col items-center text-center sm:text-left sm:items-start sm:max-w-96">
             <p className="mb-5 sm:text-lg animate-slide-top delay-600">
               Yillar davomida tinchlik bermagan{" "}
-              <span className="underline font-medium">muammoni</span> atiga bir
-              necha qator kod bilan{" "}
-              <span className="underline font-medium">hal qilishimga</span>{" "}
+              <span className="text-xs">
+                <Highlighter action="highlight" color="#FF9800">
+                  <span className="text-lg">muammoni</span>{" "}
+                </Highlighter>{" "}
+              </span>
+              atiga bir necha qator kod bilan{" "}
+              <span className="text-xs">
+                {" "}
+                <Highlighter action="underline" color="#87CEFA">
+                  <span className="text-lg">hal qilishimga</span>
+                </Highlighter>{" "}
+              </span>{" "}
               ishonasizmi?
             </p>
             <div className="flex gap-5">
               <Link
                 className={`${buttonVariants({
                   variant: "default",
-                })} animate-slide-top delay-900`}
+                })} animate-slide-top delay-900 text-lg`}
                 href="/projects/"
               >
-                Yechimlar
+                <HyperText>Yechimlar</HyperText>
               </Link>
               <Link
                 className={`${buttonVariants({
